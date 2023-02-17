4dce282f480b187b51a9778a069e48fd2800fb97
polash016 <robin.shrkr@gmail.com>
2023-02-18 05:37:38 +0600

    calculate on pentagon and ellipses button done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// blog button and new html page
+document.getElementById('btn-blog').addEventListener('click', function(){
+    window.location.href = "blog.html"
+})
+
 let serial = 0;
 // calcultate on triangle button
 
@@ -30,7 +35,7 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
     // rectangleIInner.innerText = rectangleI; 
 getSetElement('rectangle-w-inner', 'rectangle-i-inner', rectangleW, rectangleI)
     const total = parseFloat(rectangleW) * parseFloat(rectangleI);
-    addInnerElement(number, nameRectangle, total);
+    addInnerElement(serial, nameRectangle, total);
     if(typeof rectangleW !== 'number' && rectangleI !== 'number'){
         alert('Please give valid number')
     }
@@ -39,13 +44,13 @@ getSetElement('rectangle-w-inner', 'rectangle-i-inner', rectangleW, rectangleI)
 });
 // calculate on Parallelogram button
 document.getElementById('btn-parallelogram').addEventListener('click', function(){
-    let number = serial + 1;
+    serial += 1;
     const nameParallelogram = document.getElementById('name-parallelogram').innerText;
     const parallelogramB = document.getElementById('parallelogram-b').innerText;
     const parallelogramH = document.getElementById('parallelogram-h').innerText;
     getSetElement('parallelogram-b-inner', 'parallelogram-h-inner', parallelogramB, parallelogramH);
     const total = parseFloat(parallelogramB )* parseFloat(parallelogramH);
-    addInnerElement(number, nameParallelogram, total);
+    addInnerElement(serial, nameParallelogram, total);
 
 });
 // calculate on Rhombus button
@@ -56,9 +61,29 @@ document.getElementById('btn-rhombus').addEventListener('click', function(){
     const rhombusD2 = document.getElementById('rhombus-d2').innerText;
     getSetElement('rhombus-d1-inner', 'rhombus-d2-inner', rhombusD1, rhombusD2);
     const total = 0.5 * parseFloat(rhombusD1) * parseFloat(rhombusD2);
-    addInnerElement(number, nameRhombus, total);
+    addInnerElement(serial, nameRhombus, total);
 });
-
+// calculate on Pentagon button
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    serial += 1;
+    const namePentagon = document.getElementById('name-pentagon').innerText;
+    const pentagonP = document.getElementById('pentagon-p').innerText;
+    const pentagonB = document.getElementById('pentagon-b').innerText;
+    getSetElement('pentagon-p-inner', 'pentagon-b-inner', pentagonP, pentagonB);
+    const total = 0.5 * parseFloat(pentagonP) * parseFloat(pentagonB);
+    addInnerElement(serial, namePentagon, total);
+});
+// calculate on Ellipse button
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    serial += 1;
+    const nameEllipse = document.getElementById('name-ellipse').innerText;
+    const ellipseA = document.getElementById('ellipse-a').innerText;
+    const ellipseB = document.getElementById('ellipse-b').innerText;
+    getSetElement('ellipse-a-inner', 'ellipse-b-inner', ellipseA, ellipseB);
+    const total = (Math.PI).toFixed(2) * parseFloat(ellipseA) * parseFloat(ellipseB);
+    const newTotal = total.toFixed(2);
+    addInnerElement(serial, nameEllipse, newTotal);
+})
 
 function getSetElement(getField1, getField2, setField1,setField2){
     const rectangleWInner = document.getElementById(getField1);
