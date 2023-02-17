21d85988a9b85bb0fa0a70cdf6d7641ca78b7542
polash016 <robin.shrkr@gmail.com>
2023-02-18 01:20:58 +0600

    calculate on rectangle button done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let serial = 0;
-// calcultate triangle button
+// calcultate on triangle button
 
 document.getElementById('btn-calculate-first').addEventListener('click', function(){
     let number = serial + 1;
@@ -12,9 +12,33 @@ document.getElementById('btn-calculate-first').addEventListener('click', functio
     triangleHInner.innerText = triangleH; 
     const total = 0.5 * parseFloat(triangleB) * parseFloat(triangleH);
     addInnerElement('table-container', number, nameTriangle, total);
+    if(typeof triangleB !== 'number' && triangleH !== 'number'){
+        alert('Please give valid number')
+    }
+});
+// calculate on rectangle button
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    let number = serial + 1;
+    const nameRectangle = document.getElementById('name-rectangle').innerText;
+    const rectangleW = document.getElementById('input-field-w-rectangle').value;
+    const rectangleI = document.getElementById('input-field-i-rectangle').value;
+    const rectangleWInner = document.getElementById('rectangle-w-inner');
+    const rectangleIInner= document.getElementById('rectangle-i-inner');
+    rectangleWInner.innerText = rectangleW;
+    rectangleIInner.innerText = rectangleI; 
+    const total = parseFloat(rectangleW) * parseFloat(rectangleI);
+    addInnerElement('table-container', number, nameRectangle, total);
+    if(typeof triangleB !== 'number' && triangleH !== 'number'){
+        alert('Please give valid number')
+    }
 
     
 });
+function getInnerElement(titleId, element1, element2){
+    const titleElement = document.getElementById('name-rectangle').innerText;
+    const firstElement = document.getElementById('rectangle-w-inner');
+    const secondElement = document.getElementById('rectangle-i-inner');
+};
 function addInnerElement(containerId, number, name, total){
     const container = document.getElementById(containerId);
     
