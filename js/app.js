d191da7efe8c34af5236bae4bdd805290cf2ed2d
polash016 <robin.shrkr@gmail.com>
2023-02-18 05:03:33 +0600

    calculate on rhombus button added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let serial = 0;
 // calcultate on triangle button
 
 document.getElementById('btn-calculate-first').addEventListener('click', function(){
-    let number = serial + 1;
+     serial += 1;
     const nameTriangle = document.getElementById('name-triangle').innerText;
     const triangleB = document.getElementById('input-field-b-triangle').value;
     const triangleH = document.getElementById('input-field-h-triangle').value;
@@ -12,14 +12,15 @@ document.getElementById('btn-calculate-first').addEventListener('click', functio
     // triangleHInner.innerText = triangleH; 
     getSetElement('triangle-b-inner', 'triangle-h-inner', triangleB, triangleH)
     const total = 0.5 * parseFloat(triangleB) * parseFloat(triangleH);
-    addInnerElement('table-container', number, nameTriangle, total);
+    addInnerElement(number, nameTriangle, total);
     if(typeof triangleB !== 'number' && triangleH !== 'number'){
         alert('Please give valid number')
+        return;
     };
 });
 // calculate on rectangle button
 document.getElementById('btn-rectangle').addEventListener('click', function(){
-    let number = serial + 1;
+    serial += 1;
     const nameRectangle = document.getElementById('name-rectangle').innerText;
     const rectangleW = document.getElementById('input-field-w-rectangle').value;
     const rectangleI = document.getElementById('input-field-i-rectangle').value;
@@ -49,7 +50,7 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
 });
 // calculate on Rhombus button
 document.getElementById('btn-rhombus').addEventListener('click', function(){
-    let number = serial + 1;
+     serial += 1;
     const nameRhombus = document.getElementById('name-rhombus').innerText;
     const rhombusD1 = document.getElementById('rhombus-d1').innerText;
     const rhombusD2 = document.getElementById('rhombus-d2').innerText;
@@ -57,27 +58,7 @@ document.getElementById('btn-rhombus').addEventListener('click', function(){
     const total = 0.5 * parseFloat(rhombusD1) * parseFloat(rhombusD2);
     addInnerElement(number, nameRhombus, total);
 });
-// calculate on Pentagon button
-document.getElementById('btn-pentagon').addEventListener('click', function(){
-    let number = serial + 1;
-    const namePentagon = document.getElementById('name-pentagon').innerText;
-    const pentagonP = document.getElementById('pentagon-p').innerText;
-    const pentagonB = document.getElementById('pentagon-b').innerText;
-    getSetElement('pentagon-p-inner', 'pentagon-b-inner', pentagonP, pentagonB);
-    const total = 0.5 * parseFloat(pentagonP) * parseFloat(pentagonB);
-    addInnerElement(number, namePentagon, total);
-});
-// calculate on Ellipse button
-document.getElementById('btn-ellipse').addEventListener('click', function(){
-    let number = serial + 1;
-    const nameEllipse = document.getElementById('name-ellipse').innerText;
-    const ellipseA = document.getElementById('ellipse-a').innerText;
-    const ellipseB = document.getElementById('ellipse-b').innerText;
-    getSetElement('ellipse-a-inner', 'ellipse-b-inner', ellipseA, ellipseB);
-    const total = (Math.PI).toFixed(2) * parseFloat(ellipseA) * parseFloat(ellipseB);
-    const newTotal = total.toFixed(2);
-    addInnerElement(number, nameEllipse, newTotal);
-})
+
 
 function getSetElement(getField1, getField2, setField1,setField2){
     const rectangleWInner = document.getElementById(getField1);
--- js/common-function.js
+++ js/common-function.js
@@ -1,11 +1,23 @@
 
-let colorsArray = ['#fda4af', '#f9a8d4', '#f0abfc','#d8b4fe', '#c4b5fd', '#a5b4fc', '#7dd3fc', '#67e8f9', '#5eead4','#bef264']
+let colorsArray = ['#fda4af', '#f9a8d4', '#f0abfc','#d8b4fe', '#c4b5fd', '#a5b4fc', '#7dd3fc', '#67e8f9', '#5eead4','#bef264'];
+
+// function changeColor(){
+//     let i = Math.floor(Math.random()*10);
+//     // document.querySelectorAll('.card').style.backgroundColor = colorsArray[i];
+//     const cards = document.querySelectorAll('.card');
+//     for(const item of cards){
+//         item.style.backgroundColor = colorsArray[i];
+//         console.log(item)
+//     }
+// }
 function changeColor(){
-    let i = Math.floor(Math.random()*10);
+    const cards = document.querySelectorAll('.items');
+    let i = Math.floor(Math.random()*cards.length);
     // document.querySelectorAll('.card').style.backgroundColor = colorsArray[i];
-    const cards = document.querySelectorAll('.card');
-    for(single of cards){
-        single.style.backgroundColor = colorsArray[i];
+    
+    for(const item of cards){
+        item.style.backgroundColor = colorsArray[i];
+        console.log(item)
     }
 }
 // function baseColor(){
